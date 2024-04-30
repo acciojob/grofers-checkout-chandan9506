09bb77a5e23c7413376d896dd3eec7ba53a2aa8d
chandan9506 <152258839+chandan9506@users.noreply.github.com>
2024-04-30 09:06:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,23 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+
+	let table = document.querySelector("table");
+	let prices = document.querySelectorAll(".price");	
+
+    let sum = 0;
+    for(let i=0;i<prices.length;i++){
+        sum = sum +Number(prices[i].innerText);
+    }
+    
+	let newRow = document.createElement("tr");
+    newRow.innerHTML=`
+        <td>Total Price = ${sum}</td>
+        `;
+    table.append(newRow);
+
+
+	
   
 };
 
